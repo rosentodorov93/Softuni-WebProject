bbd4ca3d46636196c31bf71d8f1577407ad3c110
Rosen Todorov <rosen.vt55@gmail.com>
2022-10-12 19:41:04 +0300

    Added data layer in Infrastructure

--- FitnessDiary/FitnessDiary.Infrastructure/Data/ApplicationDbContext.cs
+++ FitnessDiary/FitnessDiary.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace FitnessDiary.Data
+namespace FitnessDiary.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- FitnessDiary/FitnessDiary.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ FitnessDiary/FitnessDiary.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace FitnessDiary.Data.Migrations
+namespace FitnessDiary.Infrastructure.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
