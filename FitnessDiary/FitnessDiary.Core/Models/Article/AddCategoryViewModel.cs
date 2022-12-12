aa5853248536d7b5ced7894e8511c90d3f7bd27a
Rosen Todorov <rosen.vt55@gmail.com>
2022-12-12 10:13:14 +0200

    Added js validation in add and edit article views

---
+++ FitnessDiary/FitnessDiary.Core/Models/Article/AddCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessDiary.Core.Models.Article
+{
+    public class AddCategoryViewModel
+    {
+        [Required]
+        [StringLength(80,MinimumLength = 4)]
+        public string Name { get; set; } = null!;
+    }
+}
--- FitnessDiary/FitnessDiary.Core/Models/Article/CategoryViewModel.cs
+++ FitnessDiary/FitnessDiary.Core/Models/Article/CategoryViewModel.cs
@@ -1,8 +1,8 @@
 ﻿namespace FitnessDiary.Core.Models.Article
 {
-    public class CategoryViewModel
+    public class CategoryViewModel : AddCategoryViewModel
     {
         public string Id { get; set; } = null!;
-        public string  Name { get; set; } =null!;
+        
     }
 }
